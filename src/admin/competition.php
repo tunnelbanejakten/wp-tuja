4ce2649b50dbfc855fc419fa296dee04fabce469
Mikael Svensson <mikael.svensson@klarna.com>
2018-11-23 09:31:39 +0100

    Less cluttered front page

--- src/admin/competition.php
+++ src/admin/competition.php
@@ -29,43 +29,53 @@ $groups = $db_groups->get_all_in_competition($competition->id);
     <h1>Tunnelbanejakten</h1>
     <h2>Tävling <?= $competition->name ?></h2>
     <h3>Formulär</h3>
+    <table>
+        <tbody>
+        <?php
+        foreach ($forms as $form) {
+            $url = add_query_arg(array(
+                'tuja_view' => 'form',
+                'tuja_form' => $form->id
+            ));
+            printf('' .
+                '<tr>' .
+                '<td><a href="%s">%s</a></td>' .
+                '<td><code>[tuja_form form="%d"]</code></td>' .
+                '<td><code>[tuja_create_group form="%d"]</code></td>' .
+                '<td><code>[tuja_points form="%d"]</code></td>' .
+                '</tr>', $url, $form->name, $form->id, $form->id, $form->id);
+        }
+        ?>
+        </tbody>
+    </table>
+    <input type="text" name="tuja_form_name"/>
+    <button type="submit" name="tuja_action" value="form_create">Skapa</button>
+    <h3>Ställning</h3>
     <?php
     $review_url = add_query_arg(array(
         'tuja_competition' => $competition->id,
         'tuja_view' => 'review'
     ));
     printf('<p><a href="%s">Gå igenom okontrollerade svar</a></p>', $review_url);
-    foreach ($forms as $form) {
-        $url = add_query_arg(array(
-            'tuja_view' => 'form',
-            'tuja_form' => $form->id
-        ));
-        printf('<p><a href="%s">%s</a> Shortcode: <code>[tuja_form form="%d"]</code></p>', $url, $form->name, $form->id);
-    }
     ?>
-    <input type="text" name="tuja_form_name"/>
-    <button type="submit" name="tuja_action" value="form_create">Skapa</button>
-    <h3>Ställning</h3>
-    <?php
-    $calculator = new ScoreCalculator($competition->id, $db_question, $db_response, $db_groups, $db_points);
-    $score_board = $calculator->score_board();
-    usort($score_board, function ($a, $b) {
-        return $b['score'] - $a['score'];
-    });
-    foreach ($score_board as $team_score) {
-        printf('<p>%s: %d p</p>', htmlspecialchars($team_score['group_name']), $team_score['score']);
-    }
-    ?>
-    <h3>Lag</h3>
-    <?php
-    foreach ($groups as $group) {
-        $group_url = add_query_arg(array(
-            'tuja_group' => $group->id,
-            'tuja_view' => 'group'
-        ));
-        printf('<p><a href="%s">%s</a></p>', $group_url, htmlspecialchars($group->name));
-    }
-    ?>
+    <table>
+        <tbody>
+        <?php
+        $calculator = new ScoreCalculator($competition->id, $db_question, $db_response, $db_groups, $db_points);
+        $score_board = $calculator->score_board();
+        usort($score_board, function ($a, $b) {
+            return $b['score'] - $a['score'];
+        });
+        foreach ($score_board as $team_score) {
+            $group_url = add_query_arg(array(
+                'tuja_group' => $team_score['group_id'],
+                'tuja_view' => 'group'
+            ));
+            printf('<tr><td><a href="%s">%s</a></td><td>%d p</td></tr>', $group_url, htmlspecialchars($team_score['group_name']), $team_score['score']);
+        }
+        ?>
+        </tbody>
+    </table>
     <input type="text" name="tuja_group_name"/>
     <select name="tuja_group_type">
         <option value="participant">Tävlande</option>
