6811b70f25e544986b66ed636541e60ca74a6ec1
Mattias Forsman <forsman1991@gmail.com>
2019-02-03 18:25:36 +0100

    Routing fixes

--- src/admin/views/index.php
+++ src/admin/views/index.php
@@ -1,25 +1,27 @@
 <?php namespace tuja\admin; ?>
 
-<form method="post" action="<?= add_query_arg() ?>">
+<div class="tuja tuja-view-index">
+	<form method="post" action="<?= add_query_arg() ?>">
 
-    <h1>Tävlingar</h1>
-    <?php
-    $competitions = $db_competition->get_all();
-    foreach ($competitions as $competition) {
-        $url = add_query_arg(array(
-            'tuja_view' => 'competition',
-            'tuja_competition' => $competition->id
-        ));
-        printf('<p><a href="%s">%s</a></p>', $url, $competition->name);
-    }
-    ?>
+		<h1>Tävlingar</h1>
+		<?php
+		$competitions = $db_competition->get_all();
+		foreach ($competitions as $competition) {
+			$url = add_query_arg(array(
+				'tuja_view' => 'competition',
+				'tuja_competition' => $competition->id
+			));
+			printf('<p><a href="%s">%s</a></p>', $url, $competition->name);
+		}
+		?>
 
-    <input type="text" name="tuja_competition_name"/>
-    <button type="submit" name="tuja_action" value="competition_create">Skapa</button>
-</form>
-<?php
-$url = add_query_arg(array(
-    'tuja_view' => 'Settings'
-));
-printf('<p><a href="%s">Inställningar</a></p>', $url);
-?>
+		<input type="text" name="tuja_competition_name"/>
+		<button type="submit" name="tuja_action" value="competition_create">Skapa</button>
+	</form>
+	<?php
+	$url = add_query_arg(array(
+		'tuja_view' => 'Settings'
+	));
+	printf('<p><a href="%s">Inställningar</a></p>', $url);
+	?>
+</div>
