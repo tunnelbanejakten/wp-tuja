67260fc2299289ecf1cc1658489ccd84631b0999
Mikael Svensson <mikael.svensson@klarna.com>
2019-04-22 12:42:22 +0200

    Core MMS import #79

--- src/admin/MessagesImport.php
+++ src/admin/MessagesImport.php
@@ -4,7 +4,6 @@ namespace tuja\admin;
 
 use tuja\data\store\CompetitionDao;
 use tuja\util\ImageManager;
-use tuja\util\MessageImporter;
 use tuja\util\SmsBackupRestoreXmlFileProcessor;
 use tuja\data\store\PersonDao;
 use tuja\data\store\MessageDao;
@@ -20,28 +19,21 @@ class MessagesImport {
 		$this->competition = $db_competition->get( $_GET['tuja_competition'] );
 		if (!$this->competition) {
 			print 'Could not find competition';
+
 			return;
 		}
-		
-	}
 
+	}
 
 	public function handle_post() {
 		if(!isset($_POST['tuja_points_action'])) return;
-		
-		if($_POST['tuja_points_action'] === 'import') {
+
+		if ( $_POST['tuja_points_action'] === 'import' ) {
 			$mms_messages = $this->get_mms_messages_to_import();
-		
+
 			if (isset($mms_messages)) {
 				$im = new ImageManager();
 
-				$person_dao  = new PersonDao();
-				$message_dao = new MessageDao();
-		
-				$importer = new MessageImporter(
-					$message_dao,
-					$person_dao->get_all_in_competition($this->competition->id));
-		
 				foreach ($mms_messages as $mms) {
 					$text_value = join('. ', $mms->texts);
 		
@@ -53,9 +45,10 @@ class MessagesImport {
 						} catch (Exception $e) {
 							printf('<p>Kunde inte importera: %s</p>', $e->getMessage());
 						}
+
 						return $carry;
 					}, []);
-		
+
 					try {
 						$message = $importer->import($text_value, $image_value, $mms->from, $mms->date);
 						printf('<p>Importerade bilderna %s med id=%s</p>',
@@ -79,25 +72,52 @@ class MessagesImport {
 	}
 
 
-	public function get_mms_messages_to_import()
-	{
+	public function get_mms_messages_to_import() {
 		$only_recent = $_POST['tuja_import_onlyrecent'] === 'yes';
-	
+
 		update_option('tuja_message_import_only_recent', $only_recent ? 'yes' : null);
-	
+
 		$date_limit = $only_recent ? new DateTime('yesterday') : null;
-	
+
 		$importer = new SmsBackupRestoreXmlFileProcessor('.', $date_limit);
-	
+
 		if (file_exists($_FILES['tuja_import_file']['tmp_name'])) {
 			return $importer->process($_FILES['tuja_import_file']['tmp_name']);
 		} else {
 			$import_url = $_POST['tuja_import_url'];
 			if (!empty($import_url)) {
 				update_option('tuja_message_import_url', $import_url);
+
 				return $importer->process($import_url);
 			}
 		}
+
 		return null;
 	}
+
+	public function import( $text, $image_ids, $sender, $timestamp, $group_id ) {
+		$message_id = sprintf( '%s,%s', $sender, $timestamp->format( DateTime::ISO8601 ) );
+
+		if ( $this->message_dao->exists( self::SOURCE, $message_id ) ) {
+			throw new Exception( sprintf( 'Meddelande %s har redan importerats.', $message_id ) );
+		}
+
+		$message                    = new Message();
+		$message->form_question_id  = null;
+		$message->group_id          = $group_id;
+		$message->text              = $text;
+		$message->image_ids         = $image_ids;
+		$message->source            = self::SOURCE;
+		$message->source_message_id = $message_id;
+		$message->date_received     = $timestamp;
+
+		$res = $this->message_dao->create( $message );
+
+		if ( $res === false ) {
+			throw new Exception( sprintf( 'Sparade inte meddelande %s i databasen pga. databasfel.', $message->source_message_id ) );
+		}
+
+		return $message;
+	}
+
 }
