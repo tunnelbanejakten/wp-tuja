32d7f2ff413fce1f186d42c4de9715a2408e4949
Mikael Svensson <mikael.svensson@klarna.com>
2019-11-07 19:32:57 +0100

    Use shortcode component to edit group #141

--- src/frontend/GroupEditor.php
+++ src/frontend/GroupEditor.php
@@ -6,6 +6,7 @@ namespace tuja\frontend;
 use tuja\data\model\Group;
 use tuja\data\store\GroupDao;
 use tuja\frontend\router\GroupHomeInitiator;
+use tuja\view\EditGroupShortcode;
 
 class GroupEditor extends FrontendView {
 	private $group_key;
@@ -18,8 +19,20 @@ class GroupEditor extends FrontendView {
 	}
 
 	function render() {
+		global $wpdb;
+
 		$group = $this->get_group();
+
+		$component = new EditGroupShortcode(
+			$wpdb,
+			$group->random_id,
+			false,
+			true );
+
+		$form = $component->render();
+
 		$home_link = GroupHomeInitiator::link( $group );
+
 		include( 'views/group-editor.php' );
 	}
 
--- src/frontend/views/group-editor.php
+++ src/frontend/views/group-editor.php
@@ -1,4 +1,5 @@
 <h1>Ändra anmälan för <?= $group->name ?></h1>
+<?= $form ?>
 <p>
 	<?php printf( '<a href="%s">Tillbaka</a>', $home_link ) ?>
 </p>
