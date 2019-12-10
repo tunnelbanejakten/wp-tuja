bb5c0ce401b4800d700bacdcab2d2a2ec482e447
Mikael Svensson <mikael.svensson@klarna.com>
2019-12-10 22:07:45 +0100

    Conditionally require SSN for people in group #112

--- src/frontend/PersonEditor.php
+++ src/frontend/PersonEditor.php
@@ -60,6 +60,11 @@ class PersonEditor extends AbstractGroupView {
 		$group        = $this->group_dao->get( $person->group_id );
 		$is_read_only = ! $this->is_edit_allowed( $group );
 
+		$real_category = $group->get_derived_group_category();
+
+		$collect_contact_information = $real_category->get_rule_set()->is_contact_information_required_for_regular_group_member();
+		$collect_ssn                 = $real_category->get_rule_set()->is_ssn_required();
+
 		if ( @$_POST[ self::ACTION_BUTTON_NAME ] == self::ACTION_NAME_SAVE ) {
 			if ( ! $is_read_only ) {
 				$errors = $this->update_person( $person );
@@ -70,54 +75,70 @@ class PersonEditor extends AbstractGroupView {
 				$errors = array( '__' => 'Tyvärr så kan anmälningar inte ändras nu.' );
 			}
 
-			return $this->render_update_form( $person, $errors, $is_read_only );
+			return $this->render_update_form( $person, true, $collect_contact_information, $collect_contact_information, $collect_ssn, true, $errors, $is_read_only );
 		} else {
-			return $this->render_update_form( $person, array(), $is_read_only );
+			return $this->render_update_form( $person, true, $collect_contact_information, $collect_contact_information, $collect_ssn, true, array(), $is_read_only );
 		}
 	}
 
-	// Move to AbstractGroupView?
-	private function render_update_form($person, $errors = array(), $read_only = false): string
-	{
+	// TODO: Similar function in three classes. Move to AbstractGroupView?
+	private function render_update_form(
+		$person,
+		bool $show_name = true,
+		bool $show_email = true,
+		bool $show_phone = true,
+		bool $show_pno = true,
+		bool $show_food = true,
+		$errors = array(),
+		$read_only = false
+	): string {
 		$html_sections = [];
 
-		if (isset($errors['__'])) {
-			$html_sections[] = sprintf('<p class="tuja-message tuja-message-error">%s</p>', @$errors['__']);
+		if ( isset( $errors['__'] ) ) {
+			$html_sections[] = sprintf( '<p class="tuja-message tuja-message-error">%s</p>', @$errors['__'] );
 		}
 
-		$person_name_question = new FieldText( 'Namn', null, $read_only );
-		$html_sections[]      = $this->render_field( $person_name_question, self::FIELD_PERSON_NAME, @$errors['name'], $person->name );
+		if ( $show_name ) {
+			$person_name_question = new FieldText( 'Namn', null, $read_only );
+			$html_sections[]      = $this->render_field( $person_name_question, self::FIELD_PERSON_NAME, @$errors['name'], $person->name );
+		}
 
-		$person_name_question = new FieldPno( 'Födelsedag och sånt', 'Vi rekommenderar att du fyller i fullständigt personnummer.', $read_only );
-		$html_sections[]      = $this->render_field( $person_name_question, self::FIELD_PERSON_PNO, @$errors['pno'], $person->pno );
+		if ( $show_pno ) {
+			$person_name_question = new FieldPno( 'Födelsedag och sånt', 'Vi rekommenderar att du fyller i fullständigt personnummer.', $read_only );
+			$html_sections[]      = $this->render_field( $person_name_question, self::FIELD_PERSON_PNO, @$errors['pno'], $person->pno );
+		}
 
-		$person_name_question = new FieldEmail( 'E-postadress' );
-		$html_sections[]      = $this->render_field( $person_name_question, self::FIELD_PERSON_EMAIL, @$errors['email'], $person->email );
+		if ( $show_email ) {
+			$person_name_question = new FieldEmail( 'E-postadress' );
+			$html_sections[]      = $this->render_field( $person_name_question, self::FIELD_PERSON_EMAIL, @$errors['email'], $person->email );
+		}
 
-		$person_name_question = new FieldPhone( 'Telefonnummer' );
-		$html_sections[]      = $this->render_field( $person_name_question, self::FIELD_PERSON_PHONE, @$errors['phone'], $person->phone );
+		if ( $show_phone ) {
+			$person_name_question = new FieldPhone( 'Telefonnummer' );
+			$html_sections[]      = $this->render_field( $person_name_question, self::FIELD_PERSON_PHONE, @$errors['phone'], $person->phone );
+		}
 
-		$person_name_question = new FieldText( 'Allergier och matönskemål', 'Arrangemanget är köttfritt och nötfritt. Fyll i här om du har ytterligare behov.', $read_only );
-		$html_sections[]      = $this->render_field( $person_name_question, self::FIELD_PERSON_FOOD, @$errors['food'], $person->food );
-
+		if ( $show_food ) {
+			$person_name_question = new FieldText( 'Allergier och matönskemål', 'Arrangemanget är köttfritt och nötfritt. Fyll i här om du har ytterligare behov.', $read_only );
+			$html_sections[]      = $this->render_field( $person_name_question, self::FIELD_PERSON_FOOD, @$errors['food'], $person->food );
+		}
 
 		if ( ! $read_only ) {
-			$html_sections[] = sprintf('<div class="tuja-buttons"><button type="submit" name="%s" value="%s">%s</button></div>',
+			$html_sections[] = sprintf( '<div class="tuja-buttons"><button type="submit" name="%s" value="%s">%s</button></div>',
 				self::ACTION_BUTTON_NAME,
 				self::ACTION_NAME_SAVE,
-				'Spara');
+				'Spara' );
 		} else {
-			$html_sections[] = sprintf('<p class="tuja-message tuja-message-error">%s</p>',
-				sprintf('Du kan inte längre ändra din anmälan. Kontakta <a href="mailto:%s">%s</a> om du behöver ändra något.',
-					get_bloginfo('admin_email'),
-					get_bloginfo('admin_email')));
+			$html_sections[] = sprintf( '<p class="tuja-message tuja-message-error">%s</p>',
+				sprintf( 'Du kan inte längre ändra din anmälan. Kontakta <a href="mailto:%s">%s</a> om du behöver ändra något.',
+					get_bloginfo( 'admin_email' ),
+					get_bloginfo( 'admin_email' ) ) );
 		}
 
-		return sprintf('<form method="post">%s</form>', join($html_sections));
+		return sprintf( '<form method="post">%s</form>', join( $html_sections ) );
 	}
 
-	private function update_person($person)
-	{
+	private function update_person( $person ) {
 		$posted_values = [
 			'name'  => $_POST[ self::FIELD_PERSON_NAME ],
 			'email' => $_POST[ self::FIELD_PERSON_EMAIL ],
@@ -152,6 +173,7 @@ class PersonEditor extends AbstractGroupView {
 				$validation_errors['__'] = 'Alla ändringar kunde inte sparas.';
 			}
 		}
+
 		return $validation_errors;
 	}
 }
