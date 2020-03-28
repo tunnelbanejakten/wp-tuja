bc0a210e50a72a12843cc72af7aa0a93ed8c2d44
Mikael Svensson <mikael.svensson@klarna.com>
2020-03-28 18:27:59 +0100

    Payment page #108

--- src/frontend/views/group-payment.php
+++ src/frontend/views/group-payment.php
@@ -7,7 +7,7 @@ use tuja\util\Strings;
 	<?php printf( Strings::get( 'groups_payment.to_pay', number_format( $amount, 2, ',', ' ' ) ) ); ?>
 </p>
 <p>
-	<small><?php printf( Strings::get( 'groups_payment.fee_model', $description, $email_link ) ); ?></small>
+    <small><?php printf( Strings::get( 'groups_payment.fee_model', $description, $email_link ) ); ?></small>
 </p>
 
 <h2><?= Strings::get( 'groups_payment.swish.header' ) ?></h2>
@@ -17,22 +17,20 @@ if ( ! empty( $swish_qr_code_image_url ) ) {
 	printf( '<div class="tuja-swish-qr-code-wrapper"><img class="tuja-swish-qr-code" src="%s" alt="QR-kod som kan scannas av Swish-appen"></div>', $swish_qr_code_image_url );
 }
 ?>
-<table class="tuja-swish-details">
-    <tbody>
-    <tr>
-        <th><?= Strings::get('groups_payment.swish.payee') ?></th>
-        <td><?= $swish_payee ?></td>
-    </tr>
-    <tr>
-        <th><?= Strings::get('groups_payment.swish.message') ?></th>
-        <td><?= $swish_message ?></td>
-    </tr>
-    <tr>
-        <th><?= Strings::get('groups_payment.swish.amount') ?></th>
-        <td><?= $amount ?></td>
-    </tr>
-    </tbody>
-</table>
+<div class="tuja-swish-details">
+    <div>
+        <div><?= Strings::get( 'groups_payment.swish.payee' ) ?></div>
+        <div><?= $swish_payee ?></div>
+    </div>
+    <div>
+        <div><?= Strings::get( 'groups_payment.swish.message' ) ?></div>
+        <div><?= $swish_message ?></div>
+    </div>
+    <div>
+        <div><?= Strings::get( 'groups_payment.swish.amount' ) ?></div>
+        <div><?= $amount ?></div>
+    </div>
+</div>
 
 <h2><?= Strings::get( 'groups_payment.giro.header' ) ?></h2>
 <p><?= Strings::get( 'groups_payment.giro.body_text', $email_link ) ?></p>
