09547f0c92731bde56455d928c2b1ce217d02317
Mikael Svensson <mikael.svensson@klarna.com>
2021-09-24 07:32:10 +0200

    Fix syntax error #242

--- src/assets/js/admin-formgenerator.js
+++ src/assets/js/admin-formgenerator.js
@@ -11,24 +11,23 @@ var tujaFormGenerator = (function () {
         if (!schema) {
           return
         }
-          var editor = new JSONEditor(el, {
-            form_name_root: el.dataset.rootName || ('JSONEditor_' + el.dataset.fieldId),
-            schema: schema,
-            no_additional_properties: true,
-            prompt_before_delete: false,
-            disable_array_reorder: true,
-            disable_edit_json: true,
-            disable_properties: true,
-            disable_collapse: true,
-            disable_array_delete_all_rows: true,
-            disable_array_delete_last_row: true
-          })
-          editor.setValue(JSON.parse(el.dataset.values))
-          editor.on('change', function () {
-            var conf = this.getValue()
-            $('#' + el.dataset.fieldId).val(JSON.stringify(conf))
-          })
-        }
+        var editor = new JSONEditor(el, {
+          form_name_root: el.dataset.rootName || ('JSONEditor_' + el.dataset.fieldId),
+          schema: schema,
+          no_additional_properties: true,
+          prompt_before_delete: false,
+          disable_array_reorder: true,
+          disable_edit_json: true,
+          disable_properties: true,
+          disable_collapse: true,
+          disable_array_delete_all_rows: true,
+          disable_array_delete_last_row: true
+        })
+        editor.setValue(JSON.parse(el.dataset.values))
+        editor.on('change', function () {
+          var conf = this.getValue()
+          $('#' + el.dataset.fieldId).val(JSON.stringify(conf))
+        })
       })
     }
   }
