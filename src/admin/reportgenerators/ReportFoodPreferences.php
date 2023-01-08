8424cd66f8841f6711641a141b56c63324f0d514
Mikael Svensson <mikael.svensson@klarna.com>
2023-01-08 09:34:00 +0100

    Improve allergen report when comma-separated list can be assumed #346

--- src/admin/reportgenerators/ReportFoodPreferences.php
+++ src/admin/reportgenerators/ReportFoodPreferences.php
@@ -17,41 +17,108 @@ class ReportFoodPreferences extends AbstractReport {
 	function get_rows(): array {
 		$people = $this->person_dao->get_all_in_competition( $this->competition->id );
 
-		$rows = array_map( function ( $value ) {
-			return [ 'value' => $value ];
-		}, array_unique( array_filter( array_map( function ( Person $person ) {
-			return $person->food;
-		}, $people ) ) ) );
+		$rows = array_map(
+			function ( $value ) {
+				return array( 'value' => $value );
+			},
+			array_filter(
+				array_map(
+					function ( Person $person ) {
+						return $person->food;
+					},
+					$people
+				),
+				function ( $value ) {
+					return ! empty( $value );
+				}
+			)
+		);
 
 		sort( $rows );
 
 		return $rows;
 	}
 
-	function output_html( array $rows ) {
-		$summary_config = [
-			'Gluten'         => [ 'gluten' ],
-			'Laktos'         => [ 'laktos' ],
-			'Inga allergier' => [ 'nepp', 'nej' ],
-			'Vego'           => [ 'vego', 'vegetarian' ]
-		];
-		$summary        = array_map(
+	private function summary_by_keywords( array $rows ) {
+		$summary_config = array(
+			'Gluten'         => array( 'gluten' ),
+			'Laktos'         => array( 'laktos' ),
+			'Inga allergier' => array( 'nepp', 'nej' ),
+			'Vego'           => array( 'vego', 'vegetarian' ),
+		);
+		return array_map(
 			function ( $label, $keywords ) use ( $rows ) {
-				return [
+				return array(
 					'label' => $label,
-					'count' => count( array_filter( $rows, function ( $row ) use ( $keywords ) {
-						foreach ( $keywords as $keyword ) {
-							if ( strpos( strtolower( $row['value'] ), strtolower( $keyword ) ) !== false ) {
-								return true;
-							}
-						}
+					'count' => count(
+						array_filter(
+							$rows,
+							function ( $row ) use ( $keywords ) {
+								foreach ( $keywords as $keyword ) {
+									if ( strpos( strtolower( $row['value'] ), strtolower( $keyword ) ) !== false ) {
+										return true;
+									}
+								}
 
-						return false;
-					} ) )
-				];
+								return false;
+							}
+						)
+					),
+				);
 			},
 			array_keys( $summary_config ),
-			array_values( $summary_config ) );
+			array_values( $summary_config )
+		);
+	}
+
+	private function summary_by_comma_separation( array $rows ) {
+		$summary = array_reduce(
+			$rows,
+			function ( $summary, $row ) {
+				$raw_value        = $row['value'];
+				$values           = array_filter( array_map( 'trim', explode( ',', $raw_value ) ) );
+				$countable_values = array_map(
+					function ( $str ) {
+						return 'allergenen ' . $str;
+					},
+					$values
+				);
+				if ( count( $values ) > 1 ) {
+					sort( $values );
+					$countable_values[] = 'kombinationen ' . join( ', ', $values );
+				} else {
+					$countable_values[] = 'enbart ' . $values[0];
+				}
+				foreach ( $countable_values as $value ) {
+					if ( ! isset( $summary[ $value ] ) ) {
+						$summary[ $value ] = array(
+							'label' => $value,
+							'count' => 0,
+						);
+					}
+					$summary[ $value ]['count']++;
+				}
+				return $summary;
+			},
+			array()
+		);
+		usort(
+			$summary,
+			function ( $a, $b ) {
+				return $b['count'] - $a['count'];
+			}
+		);
+		return $summary;
+	}
+
+	function output_html( array $rows ) {
+		$list_all = 'true' === $_GET['tuja_reports_list_all'];
+
+		$comma_separated = 'comma_separated' === $_GET['tuja_reports_grouping'];
+		$summary         = $comma_separated
+			? $this->summary_by_comma_separation( $rows )
+			: $this->summary_by_keywords( $rows );
+
 		include( 'views/report-foodpreferences.php' );
 	}
 }
